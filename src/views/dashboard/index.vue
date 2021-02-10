b5293e5c4cc118d48fd7f02b69939ec9afa2312f
Newgithuber3 <68054659+Newgithuber3@users.noreply.github.com>
2021-02-10 18:30:56 +0800

    添加图标和用户添加页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="dashboard-container">
+  <div class="dashboard-editor-container">
     <div class="dashboard-text">name: {{ name }}</div>
     <p>{{user.username+user.id}}</p>
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData" />
+    </el-row>
   </div>
 </template>
-
 <script>
 import { mapGetters } from 'vuex'
+import LineChart from '@/components/Chart/LineChart'
+import PanelGroup from '@/components/Chart/PanelGroup'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
 
 export default {
   name: 'Dashboard',
+  components: {
+    LineChart,
+    PanelGroup
+  },
   data() {
     return {
-      user: {}
+      user: {},
+      lineChartData: lineChartData.newVisitis
     }
   },
   computed: {
@@ -26,12 +55,39 @@ export default {
   methods: {
     getUser() {
       this.user = this.$store.getters.user
+    },
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
 .dashboard {
   &-container {
     margin: 30px;
--- src/views/user/add.vue
+++ src/views/user/add.vue
@@ -1,15 +1,113 @@
 <template>
-  <div>
-    <p>hello</p>
+  <div class="container" style="margin: 0 auto; width: 600px; text-align: center;">
+    <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="ruleForm">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="ruleForm.username" />
+      </el-form-item>
+      <el-form-item label="昵称" prop="nickname">
+        <el-input v-model="ruleForm.nickname" />
+      </el-form-item>
+      <el-form-item label="手机号" prop="telephone">
+        <el-input v-model="ruleForm.telephone" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="性别" prop="gender">
+        <el-radio v-model="ruleForm.gender" label="男">男</el-radio>
+        <el-radio v-model="ruleForm.gender" label="女">女</el-radio>
+      </el-form-item>
+      <el-form-item prop="birthday">
+        <el-date-picker type="date" placeholder="选择生日日期" v-model="ruleForm.birthday" style="width: 100%;"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="住址" prop="address">
+        <el-input v-model="ruleForm.address" placeholder="请填写居住城市" />
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Add'
+  name: 'Add',
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm: {
+        username: '',
+        nickname: '',
+        telephone: '',
+        password: '',
+        checkPass: '',
+        gender: '',
+        birthday: '',
+        address: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输用户名', trigger: 'blur' },
+          { min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' }
+        ],
+        telephone: [
+          { required: true, message: '请输大陆手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度在为 11 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: 'blur' }
+        ],
+        address: [
+          { required: true, message: '请填写居住城市', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.ruleForm{
+  width: 400px;
+}
 </style>
